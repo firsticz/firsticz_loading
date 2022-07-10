4e513876bd7f9918bd47f822c68f3277856aa019
firsticz <first@thai.run>
2022-07-10 14:52:27 +0700

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author { 'firsticz' }
+description { 'Another FiveM loading screen' }
+version '0.1.0'
+
+loadscreen 'loadscreen.html'
+
+files {
+    'loadscreen.html',
+    'css/style.css',
+    'img/logo.png',
+    'js/script.js',
+}
